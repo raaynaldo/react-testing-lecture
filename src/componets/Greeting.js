ea7676cdd58bf29f72fdb8c36f8ece3d36ca4d44
raaynaldo <raaynaldo@hotmail.com>
2021-05-12 16:47:53 -0400

    fix: change conditional variable

--- src/componets/Greeting.js
+++ src/componets/Greeting.js
@@ -10,8 +10,8 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changeTextHandler && <p>It's good to see you!</p>}
-      {changeTextHandler && <p>Changed!</p>}
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
