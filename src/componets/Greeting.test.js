32d43e5b4ac7af5229986107a2ace3580f8ed8af
raaynaldo <raaynaldo@hotmail.com>
2021-05-12 17:01:05 -0400

    test: check if the text It's good too see you not exist after click

--- src/componets/Greeting.test.js
+++ src/componets/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
@@ -14,12 +15,38 @@ describe('Greeting component', () => {
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test("renders It's good to see you! if the button was NOT clicked", () => {
+  test('renders "It\'s good to see you!" if the button was NOT clicked', () => {
     render(<Greeting />);
 
     const outputElement = screen.getByText('good to see you', { exact: false });
     expect(outputElement).toBeInTheDocument();
   });
 
-  //   test();
+  test('renders "Changed!" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('doesn\'t render "It\'s good to see you!" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText('good to see you!', {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
+  });
 });
