cc3ffe463ebe0a38965a5f407d8a777a95b81f71
raaynaldo <raaynaldo@hotmail.com>
2021-05-12 16:32:00 -0400

    feat: add button for change text

--- src/componets/Greeting.js
+++ src/componets/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      {!changeTextHandler && <p>It's good to see you!</p>}
+      {changeTextHandler && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
