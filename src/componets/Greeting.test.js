a5d983bbfd8dfa2c9758c1bb5130a535460bf8f5
raaynaldo <raaynaldo@hotmail.com>
2021-05-12 16:48:13 -0400

    test: create test for good to see you

--- src/componets/Greeting.test.js
+++ src/componets/Greeting.test.js
@@ -13,4 +13,13 @@ describe('Greeting component', () => {
     const helloWorldElement = screen.getByText('Hello World', { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
   });
+
+  test("renders It's good to see you! if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  //   test();
 });
